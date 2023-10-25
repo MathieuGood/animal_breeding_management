76d6dfea8ded234d25f72ca780c999d361bfe45f
Mathieu <bon.mathieu@gmail.com>
2023-10-25 23:06:44 +0200

    Cleaned the page

--- pages/generate_evolution.php
+++ pages/generate_evolution.php
@@ -2,8 +2,6 @@
 
 <?php
 if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
-    $users_db = new User();
-    $count_snakes = $users_db->countSnakes();
 ?>
 <input class="button" type="button" onclick="window.location.href='index.php?page=edit_user&id=new_entry'" value="Generate population evolution">
 
