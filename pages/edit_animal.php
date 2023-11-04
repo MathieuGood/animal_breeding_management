2484dcaf4796ba2d132a0674cc6f597a7aa9dbce
Mathieu <bon.mathieu@gmail.com>
2023-11-04 17:46:47 +0100

    Built edit_animal structure with static construction

--- pages/edit_animal.php
+++ pages/edit_animal.php
@@ -46,22 +46,94 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0 && isset($choice)) {
             <?php
             $animal_columns = $animal->getColumnNamesAndInputType();
 
-            $values = $animal->getAnimalData();
+            $animal_values = $animal->getAnimalData();
+            echo $animal_values['animal_name'];
 
-            // For debugging
-            echo '<pre>'; var_dump($animal_columns); echo '</pre>';
-            echo '<pre>'; var_dump($values); echo '</pre>';
 
-            $count = 0;
-            foreach ($values as $key => $value) {
-                echo "<tr>";
-                echo "<td>".$animal_columns[$count]['label']."</td>";
-                echo '<td><input type="'.$animal_columns[$count]['html_input_type'].'" name="'.$key.'" value="'.$value.'"></td>';
-                echo "</tr>";
-                $count++;
-
-            }
+            $breeds = $animal->getAnimalBreeds();
+            echo '<pre>'; var_dump($animal_values); echo '</pre>';
             ?>
+
+            <tr>
+                <td>Breed</td>
+                <td>
+                    <select name="breed">
+                    <?php
+                    $breeds = $animal->getAnimalBreeds();
+                    foreach ($breeds as $breed) {
+                        echo '<option value="'.$breed['id_breed'].'">'.$breed['breed_name'].'</option>';
+                    }
+                    ?>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Name</td>
+                <td>
+                    <input type=text name="animal_name" value="<?php echo $animal_values['animal_name'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Sex</td>
+                <td>
+                    <select name="sex">
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Heigth</td>
+                <td>
+                    <input type=text name="animal_heigth" value="<?php echo $animal_values['animal_heigth'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Weigth</td>
+                <td>
+                    <input type=text name="animal_weight" value="<?php echo $animal_values['animal_weight'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Lifespan</td>
+                <td>
+                    <input type=text name="animal_lifespan" value="<?php echo $animal_values['animal_lifespan'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Birth</td>
+                <td>
+                    <input type=datetime-local name="birth_timestamp" value="<?php echo $animal_values['birth_timestamp'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Death</td>
+                <td>
+                    <input type=datetime-local name="death_timestamp" value="<?php echo $animal_values['death_timestamp'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Father</td>
+                <td>
+                    <input type=text name="id_father" value="<?php echo $animal_values['id_father'] ?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Mother</td>
+                <td>
+                    <input type=text name="id_mother" value="<?php echo $animal_values['id_mother'] ?>">
+                </td>
+            </tr>
+
         </table></p>
             <input class="button redbutton" type="button" onclick="window.location.href='index.php?page=animal_list'" value="Cancel">
             <input class="button" type="submit" name="form_submit" value="Submit">
