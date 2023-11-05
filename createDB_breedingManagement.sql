7852817fb21643a6778be6e5d0dccc69b5689cd3
Mathieu <bon.mathieu@gmail.com>
2023-11-05 23:35:48 +0100

    Added default 0 value for id_father and id_mother

--- createDB_breedingManagement.sql
+++ createDB_breedingManagement.sql
@@ -35,8 +35,8 @@ CREATE TABLE animal (
     animal_lifespan DECIMAL DEFAULT NULL,
     birth_timestamp TIMESTAMP DEFAULT 0,
     death_timestamp TIMESTAMP DEFAULT 0,
-    id_father INT DEFAULT NULL,
-    id_mother INT DEFAULT NULL
+    id_father INT DEFAULT 0,
+    id_mother INT DEFAULT 0
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 INSERT INTO animal (
@@ -53,10 +53,10 @@ INSERT INTO animal (
     id_mother
     ) VALUES 
     (1, 1, 'Desmond', 'M', 150, 3.2, 10.5, '2023-10-20 19:30:00', 0, 3, 2),
-    (2, 2, 'Mary', 'F', 162, 2.1, 9.1, '2023-10-03 19:00:00', 0, NULL, NULL),
-    (3, 2, 'Jasper', 'M', 107, 1.9, 8.5, '2023-10-01 22:11:00', 0, NULL, NULL),
+    (2, 2, 'Mary', 'F', 162, 2.1, 9.1, '2023-10-03 19:00:00', 0, 0, 0),
+    (3, 2, 'Jasper', 'M', 107, 1.9, 8.5, '2023-10-01 22:11:00', 0, 0, 0),
     (4, 5, 'Katy', 'F', 181, 2.0, 8.7, '2023-10-30 05:20:00', 0, 1, 5),
-    (5, 1, 'Cindarella', 'F', 165, 3.5, 10.1, '2023-10-19 14:53:00', 0, NULL, NULL);
+    (5, 1, 'Cindarella', 'F', 165, 3.5, 10.1, '2023-10-19 14:53:00', 0, 0, 0);
 
 
 -- breed table : different breeds of animals
