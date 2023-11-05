748fec4a496593b0fcacdc6603391a5cc14e3f68
Mathieu <bon.mathieu@gmail.com>
2023-11-05 23:37:53 +0100

    Set date of death default value to current time

--- pages/declare_death.php
+++ pages/declare_death.php
@@ -2,13 +2,16 @@
 $id = $_GET['id'];
 if (isset($_SESSION['open']) && $_SESSION['open'] > 0 && isset($id)) {
     $animal = new Animal($id);
+    // To implement :
+    // Check if animal is already dead and print error message if so
     ?>
     <p>
     Would you like to declare the death of <?php echo $_SESSION['animal_specie']." ".$animal->getAnimalName() ?> ?
     </p>
     <form method="POST" action="">
         <p>
-            Enter date of death  <input class="" type="datetime-local" name="death_date">
+            <?php $now = date('Y-m-d\Th:i', time()); ?>
+            Enter date of death  <input class="" type="datetime-local" name="death_date" value="<?php echo $now; ?>">
         </p>
         <input class="button redbutton" type="button" onclick="window.location.href = 'index.php?page=animal_list'" value="Cancel">
         <input class="button" type="submit" name="form_submit" value="Confirm death">
