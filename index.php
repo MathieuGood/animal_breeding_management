83f55beba5520537b8d0028322cc52b4373cf149
Mathieu <bon.mathieu@gmail.com>
2023-11-06 09:12:42 +0100

    Added favicon

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="icon" href="images/snake.ico" />
         <title><?php echo ucfirst($_SESSION['animal_specie']) ?> Breeding Management</title>
     </head>
     <body>
