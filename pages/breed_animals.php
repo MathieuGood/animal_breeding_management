09e6c061bf43bf2fd79792888216c9b8fae15134
Mathieu <bon.mathieu@gmail.com>
2023-11-23 18:15:26 +0100

    Reformated document with proper indentation

--- pages/breed_animals.php
+++ pages/breed_animals.php
@@ -3,39 +3,39 @@
 <?php
 if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
     $animal = new Animal();
-?>
+    ?>
 
-<select name='male_animal' value="---">
-    <option>Select male animal</option>
+    <select name='male_animal' value="---">
+        <option>Select male animal</option>
         <?php
         // Get the list of all male animal names
         $males = $animal->getAllAliveAnimalsBySex('M');
 
         foreach ($males as $male) {
             echo '<option ';
-            echo 'value="'.$male['id_animal'].'">#'.$male['id_animal'].' '.$male['animal_name'].'</option>';
+            echo 'value="' . $male['id_animal'] . '">#' . $male['id_animal'] . ' ' . $male['animal_name'] . '</option>';
         }
         ?>
 
-</select>
+    </select>
 
-<select name='female_animal' value="---">
-    <option value='0'>Select female animal</option>
+    <select name='female_animal' value="---">
+        <option value='0'>Select female animal</option>
         <?php
         // Get the list of all male animal names
         $females = $animal->getAllAliveAnimalsAndParentsBySex('F');
 
         foreach ($females as $female) {
             echo '<option ';
-            echo 'value="'.$female['id_animal'].'">#'.$female['id_animal'].' '.$female['animal_name'].'</option>';
+            echo 'value="' . $female['id_animal'] . '">#' . $female['id_animal'] . ' ' . $female['animal_name'] . '</option>';
         }
         ?>
 
-</select>
+    </select>
 
-<input class="button" type="submit" name="start_mating" value="Animal mating">
+    <input class="button" type="submit" name="start_mating" value="Animal mating">
 
-<?php
+    <?php
 
 } else {
     header("Location: index.php?page=admin");
