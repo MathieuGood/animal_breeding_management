1a1830dfb35283402d7221ff40b8e07c0abc064d
Mathieu <bon.mathieu@gmail.com>
2023-11-24 09:10:37 +0100

    Updated method and delete unused one

--- classes/animal.class.php
+++ classes/animal.class.php
@@ -1,115 +1,137 @@
 <?php
 
-class Animal {
+class Animal
+{
 
     private $table = "animal";
     private $id;
 
-    public function __construct($id='') {
+    public function __construct($id = '')
+    {
         $this->id = $id;
     }
-    
-    public function get($col) {
+
+    public function getAllAnimals()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->select($this->table, $this->id, $col);
+        return $db_connect->sendQuery("SELECT * FROM `" . $this->table . "`", "num");
     }
 
-    public function getAllAnimals() {
+    public function getAllLiveAnimals()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT * FROM `".$this->table."`", "num");
+        return $db_connect->sendQuery(
+            "SELECT *
+               FROM `" . $this->table .
+            "` WHERE death_timestamp = '0000-00-00 00:00:00' ORDER BY id_animal DESC",
+            "num"
+        );
     }
 
-    public function getAllLiveAnimals() {
+    public function getAllDeadAnimals()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT * FROM `".$this->table."` WHERE death_timestamp = '0000-00-00 00:00:00' ORDER BY id_animal DESC", "num");
+        return $db_connect->sendQuery("SELECT * FROM `" . $this->table . "` WHERE death_timestamp != '0000-00-00 00:00:00'", "num");
     }
 
-    public function getAllDeadAnimals() {
-        $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT * FROM `".$this->table."` WHERE death_timestamp != '0000-00-00 00:00:00'", "num");
-    }
-
-    public function getAnimalBreeds() {
+    public function getAnimalBreeds()
+    {
         $db_connect = new dbConnect();
         return $db_connect->sendQuery("SELECT id_breed, breed_name FROM `breed`");
     }
 
-    public function getAllAliveAnimalsAndParentsBySex($sex) {
-    $db_connect = new dbConnect();
-    return $db_connect->sendQuery("SELECT id_animal, animal_name, id_father, id_mother FROM animal WHERE animal_sex = '".$sex."' AND death_timestamp = '0';");    
+    public function getAllAliveAnimalsAndParentsBySex($sex)
+    {
+        $db_connect = new dbConnect();
+        return $db_connect->sendQuery("SELECT id_animal, animal_name, id_father, id_mother FROM animal WHERE animal_sex = '" . $sex . "' AND death_timestamp = '0';");
     }
 
-    public function getAllAnimalNames($sex) {
+    public function getAllAnimalNames($sex)
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT id_animal, animal_name FROM `".$this->table."` WHERE `animal_sex` ='".$sex."' AND id_animal != '".$this->id."'");
+        return $db_connect->sendQuery("SELECT id_animal, animal_name FROM `" . $this->table . "` WHERE `animal_sex` ='" . $sex . "' AND id_animal != '" . $this->id . "'");
     }
 
-    public function getAnimalData() {
+    public function getCurrentAnimalData()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT * FROM `".$this->table."` WHERE `id_".$this->table."` = ".$this->id)[0];
+        return $db_connect->sendQuery("SELECT * FROM `" . $this->table . "` WHERE `id_" . $this->table . "` = " . $this->id)[0];
     }
 
-    // Get animal_name from id
-    public function getAnimalName() {
+    // Get animal_name from id (default: current animal)
+    public function getAnimalName($id = $this->id)
+    {
         $db_connect = new dbConnect();
-        $result = $db_connect->sendQuery("SELECT `animal_name` FROM `".$this->table."` WHERE id_".$this->table." = '".$this->id."'");
+        $result = $db_connect->sendQuery("SELECT `animal_name` FROM `" . $this->table . "` WHERE id_" . $this->table . " = '" . $id . "'");
         return $result[0]['animal_name'];
     }
 
-    public function getBreedName($id_breed) {
+    public function getBreedName($id_breed)
+    {
         $db_connect = new dbConnect();
-        $result = $db_connect->sendQuery("SELECT `breed_name` FROM `breed` WHERE id_breed = '".$id_breed."'");
+        $result = $db_connect->sendQuery("SELECT `breed_name` FROM `breed` WHERE id_breed = '" . $id_breed . "'");
         return $result[0]['breed_name'];
     }
 
-    public function update($col, $value) {
+    public function update($col, $value)
+    {
         $db_connect = new dbConnect();
         return $db_connect->update($this->table, $this->id, $col, $value);
     }
 
-    public function createAnimal($cols, $values) {
+    public function createAnimal($cols, $values)
+    {
         $db_connect = new dbConnect();
         return $db_connect->insertMultiple($this->table, $cols, $values);
     }
 
     // Creates a new random animal with data consistent with its breed characteristics
     // Using the createRandomAnimal() stored procedure in database
-    public function createRandomAnimal($number = 1) {
+    public function createRandomAnimal($number = 1)
+    {
         $db_connect = new dbConnect();
-        if ($number == '' or $number > 100000) { $number = 0; }
-        echo 'CALL createRandomAnimals('.$number.');';
-        return $db_connect->sendQuery('CALL createRandomAnimals('.$number.');');
+        if ($number == '' or $number > 100000) {
+            $number = 0;
+        }
+        echo 'CALL createRandomAnimals(' . $number . ');';
+        return $db_connect->sendQuery('CALL createRandomAnimals(' . $number . ');');
     }
 
-    public function declareDead($death_timestamp) {
+    public function declareDead($death_timestamp)
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("UPDATE `".$this->table."` SET `death_timestamp` = '".$death_timestamp."' WHERE `id_".$this->table."` = ".$this->id);
+        return $db_connect->sendQuery("UPDATE `" . $this->table . "` SET `death_timestamp` = '" . $death_timestamp . "' WHERE `id_" . $this->table . "` = " . $this->id);
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $db_connect = new dbConnect();
-        $query = "DELETE FROM `".$this->table."` WHERE `id_".$this->table."` = '".$id."'";
+        $query = "DELETE FROM `" . $this->table . "` WHERE `id_" . $this->table . "` = '" . $id . "'";
         $db_connect->sendQuery($query);
     }
 
-    public function countAnimals() {
+    public function countAnimals()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT COUNT(*) as count_table FROM `".$this->table."`");
+        return $db_connect->sendQuery("SELECT COUNT(*) as count_table FROM `" . $this->table . "`");
     }
 
 
 
-    public function selectAll() {
+    public function selectAll()
+    {
         $db_connect = new dbConnect();
-        return $db_connect->sendQuery("SELECT * FROM `".$this->table."`");
+        return $db_connect->sendQuery("SELECT * FROM `" . $this->table . "`");
     }
 
-    public function getColumnNames() {
+    public function getColumnNames()
+    {
         $db_connect = new dbConnect();
         return $db_connect->getColumnNames($this->table);
     }
 
-    public function getColumnNamesAndInputType() {
+    public function getColumnNamesAndInputType()
+    {
         $db_connect = new dbConnect();
         return $db_connect->getColumnNamesAndInputType($this->table);
     }
