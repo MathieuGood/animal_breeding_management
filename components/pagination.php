76277ba538d4b87e81617d7e3f2c81b3bc423f5b
Mathieu <bon.mathieu@gmail.com>
2023-12-05 00:13:01 +0100

    Minor changes

--- components/pagination.php
+++ components/pagination.php
@@ -1,5 +1,21 @@
 <!-- Pagination -->
 <!-- Modify page numbers displayed (previous and next) relatively to the current_page -->
+
+<?php
+// Send query to count the animals and store the value in SESSION
+$animals_count = $animal->countAllFilteredAndSortedAnimals($sort, $name_filter, $breed_filter, $sex_filter);
+$_SESSION['animal_count'] = $animals_count;
+
+
+// Number of pages to display all animals
+$total_page = ceil($animals_count / $rows_per_page);
+
+
+// Calculate offset to inject in query
+$offset = ($current_page - 1) * $rows_per_page;
+?>
+
+
 <nav aria-label="Page navigation for animal list">
     <ul class="pagination">
         <?php
