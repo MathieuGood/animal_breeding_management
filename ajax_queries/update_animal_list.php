04a6866843bf00b13ee9b091e80a623bfd1f756f
Mathieu <bon.mathieu@gmail.com>
2023-12-05 00:15:25 +0100

    Fully functional breed and sex filters. Still having issues with the reset settings button that does not work.

--- ajax_queries/update_animal_list.php
+++ ajax_queries/update_animal_list.php
@@ -26,7 +26,10 @@ foreach ($post_values_to_save_in_session as $value) {
 // For debugging
 var_dump($_POST);
 echo '<br>';
-
+echo '<br>';
+var_dump($_SESSION);
+echo '<br>';
+echo '<br>';
 
 // Get the values from SESSION
 $sort = $_SESSION['sort'];
@@ -44,31 +47,47 @@ $animal_count = $animal->countAllFilteredAndSortedAnimals($sort, $name_filter, $
 $_SESSION['animal_count'] = $animal_count;
 
 
+
+
+
 // Number of pages to display all animals
 $total_page = ceil($animal_count / $rows_per_page);
 
-// For debugging
-echo 'current page : ';
-var_dump($current_page);
-echo '<br>';
 
 // Calculate offset to inject in query
 $offset = ($current_page - 1) * $rows_per_page;
 
 
-// For debugging
-echo 'offset : ';
-var_dump($offset);
-echo '<br>';
-
 // Send query to get the animals
 $filtered_animal_data = $animal->getAllFilteredAndSortedAnimals($sort, $name_filter, $breed_filter, $sex_filter, $rows_per_page, $offset);
 
 $filtered_breed_list = $animal->getAllFilteredAndSortedBreeds($name_filter, $breed_filter, $sex_filter);
+
+$filtered_sex_list = $animal->getAllFilteredAndSortedSex($name_filter, $breed_filter, $sex_filter);
+echo '<br><br>';
+var_dump($filtered_sex_list);
+echo '<br><br>';
 ?>
 
-<!-- Dropdown for breed filter -->
-<div class="dropdown">
+<!-- Dropdown selector for breed filter -->
+
+<div class="btn-group">
+    <!-- Sex filter -->
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Sex
+        </button>
+        <ul class="dropdown-menu">
+            <?php
+            foreach ($filtered_sex_list as $one_sex) {
+                echo "<li><a class='dropdown-item' href='#' 
+                    onclick='filterAnimalListBySex(\"" . $one_sex['animal_sex'] . "\")'>" . $one_sex['animal_sex'] . " (" . $one_sex['sex_count'] . ")</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
+
+    <!-- Breed filter -->
     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
         Breed
     </button>
@@ -77,62 +96,75 @@ $filtered_breed_list = $animal->getAllFilteredAndSortedBreeds($name_filter, $bre
 
         foreach ($filtered_breed_list as $one_breed) {
             echo "<li><a class='dropdown-item' href='#' 
-                onclick='filterAnimalListByBreed(\"" . $one_breed['breed_name'] . "\")'>" . $one_breed['breed_name'] . " (" . $one_breed['breed_count'] . ")</a></li>";
+                    onclick='filterAnimalListByBreed(\"" . $one_breed['breed_name'] . "\")'>" . $one_breed['breed_name'] . " (" . $one_breed['breed_count'] . ")</a></li>";
         }
+
         ?>
     </ul>
 </div>
 
-<div class="reset-everything">
-    <button class="btn"></button>
+<!-- Reset filters -->
+<div class="reset-filters">
+    <button class="btn btn-secondary" onclick="resetSearchParameters()">
+        Reset all filters
+    </button>
+</div>
 
+</div>
 
-    <table>
-        <!-- Header row -->
-        <tr id="animal_list_header">
-            <th></th>
-            <?php
-            $animal_columns = $animal->getColumnNames();
-            // Display each column name
-            foreach ($animal_columns as $column) {
-                echo "<th>" . $column['label'];
-                // For debugging
-                // var_dump($column);
-                ?>
-                <div id="sort_arrows">
-                    <button name=" <?php echo $column['id_column_label']; ?>_asc"
-                        value="<?php echo $column['id_column_label']; ?> ASC"
-                        onclick="sortAnimalList('<?php echo $column['id_column_label']; ?> ASC')">&uarr;</button>
-                    <button name="<?php echo $column['id_column_label']; ?>_desc"
-                        value="<?php echo $column['id_column_label']; ?> DESC"
-                        onclick="sortAnimalList('<?php echo $column['id_column_label']; ?> DESC')">&darr;</button>
-                </div>
-                </th>
-                <?php
-            }
-            ?>
-        </tr>
-
+<table>
+    <!-- Header row -->
+    <tr id="animal_list_header">
+        <th></th>
         <?php
-        // Build row for each animal
-        foreach ($filtered_animal_data as $one_animal) {
-            echo "<tr>
+        $animal_columns = $animal->getColumnNames();
+        // Display each column name
+        foreach ($animal_columns as $column) {
+            echo "<th>" . $column['label'];
+            // For debugging
+            // var_dump($column);
+            ?>
+            <div id="sort_arrows">
+
+                <button name=" <?php echo $column['id_column_label']; ?>_asc"
+                    value="<?php echo $column['id_column_label']; ?> ASC"
+                    onclick="sortAnimalList('<?php echo $column['id_column_label']; ?> ASC')">
+                    &uarr;
+                </button>
+
+                <button name="<?php echo $column['id_column_label']; ?>_desc"
+                    value="<?php echo $column['id_column_label']; ?> DESC"
+                    onclick="sortAnimalList('<?php echo $column['id_column_label']; ?> DESC')">
+                    &darr;
+                </button>
+
+            </div>
+            </th>
+            <?php
+        }
+        ?>
+    </tr>
+
+    <?php
+    // Build row for each animal
+    foreach ($filtered_animal_data as $one_animal) {
+        echo "<tr>
             <td>
             <a href='index.php?page=declare_death&id=" . $one_animal['id_animal'] . "'>💀</a>
             <a href='index.php?page=edit_animal&choice=edit&id=" . $one_animal['id_animal'] . "'>✏️</a>
             </td>";
-            // In each column of one row, display the value of one of the animal characteristics
-            foreach ($one_animal as $value) {
-                echo "<td>" . $value . "</td>";
-            }
-            echo "</tr>";
+        // In each column of one row, display the value of one of the animal characteristics
+        foreach ($one_animal as $value) {
+            echo "<td>" . $value . "</td>";
         }
-        ?>
-
-    </table>
-
-
-    <?php
-    // Include navigation component
-    include('../components/pagination.php');
+        echo "</tr>";
+    }
     ?>
+
+</table>
+
+
+<?php
+// Include navigation component
+include('../components/pagination.php');
+?>
--- pages/animal_list.php
+++ pages/animal_list.php
@@ -6,27 +6,27 @@
 // If the user is logged in
 if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
 
-    $animal = new Animal();
-
     // On page load, reset all filters, sorting and pagination stored in $_SESSION
-    $_SESSION['sort'] = 'id_animal ASC';
-    $_SESSION['name_filter'] = '';
-    $_SESSION['breed_filter'] = '';
-    $_SESSION['sex_filter'] = '';
-    $_SESSION['current_page'] = 1;
-    $_SESSION['rows_per_page'] = 5;
+    include('ajax_queries/reset_search_parameters.php');
+
+    // Instantiate new Animal object
+    $animal = new Animal();
 
     // Display the total number of animals
     // REFRESHES ON PAGE RELOAD ONLY
     // FOR FUTURE : make it part of AJAX query when creating animals
-    $count_male = $animal->countAllAliveAnimalsBySex('M');
-    $count_female = $animal->countAllAliveAnimalsBySex('F');
-    $count_animals = $count_male + $count_female;
-    echo "Total number of " . $_SESSION['animal_specie_plural'] . " : " . $count_animals;
+
+    $sex_count = $animal->countAllAliveAnimalsBySex();
+
+    $total_count = $sex_count[1]['count'] + $sex_count[0]['count'];
+
+    echo "Total number of " . $_SESSION['animal_specie_plural'] . " : " . $total_count;
     echo "<br>";
-    echo "Male : " . $count_male;
+    // echo "Male : " . $count_male;
+    echo "Male : " . $sex_count[1]['count'];
     echo "<br>";
-    echo "Female : " . $count_female;
+    // echo "Female : " . $count_female;
+    echo "Female : " . $sex_count[0]['count'];
     echo "<br>";
 
 
@@ -44,6 +44,8 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
             value="Create random <?php echo $_SESSION['animal_specie_plural'] ?> ">
     </form>
 
+
+
     <div id="animal_list">
 
     </div>
@@ -52,15 +54,18 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
     <script>
         // When the page loads, execute sortAnimalList() with 'animal_id DESC' as default parameter 
         window.onload = function () {
+            resetSearchParameters();
+        }
+
+
+        // Set all the parameters stored in SESSION to default
+        function resetSearchParameters() {
+            $.post("ajax_queries/reset_search_parameters.php");
             sortAnimalList('id_animal DESC');
         }
 
 
-
-
-
-        
-        // When the user clicks on the button, sort according to the type parameter and reset current_page to 1
+        // AJAX function for sorting
         function sortAnimalList(type) {
             $.ajax({
                 type: "POST",
@@ -78,6 +83,41 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
         }
 
 
+        // AJAX function for filtering by breed
+        function filterAnimalListByBreed(type) {
+            $.ajax({
+                type: "POST",
+                url: "ajax_queries/update_animal_list.php",
+                data: {
+                    breed_filter: type,
+                    current_page: '1',
+                },
+                // If the ajax query returns a success, update the table
+                success: function (data) {
+                    let animal_list_table = document.getElementById("animal_list");
+                    animal_list_table.innerHTML = data;
+                }
+            });
+        }
+
+
+        // AJAX function for filtering by sex
+        function filterAnimalListBySex(type) {
+            $.ajax({
+                type: "POST",
+                url: "ajax_queries/update_animal_list.php",
+                data: {
+                    sex_filter: type,
+                    current_page: '1',
+                },
+                // If the ajax query returns a success, update the table
+                success: function (data) {
+                    let animal_list_table = document.getElementById("animal_list");
+                    animal_list_table.innerHTML = data;
+                }
+            });
+        }
+
         // When the user clicks on the button, navigate to input current_page number
         function pageAnimalList(page_number) {
             $.ajax({
@@ -93,6 +133,8 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
                 }
             });
         }
+
+
     </script>
     <?php
 } else {
