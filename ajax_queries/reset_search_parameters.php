fe11edaacfc60f35220ac7d1d84a6ad8ed40da02
Mathieu <bon.mathieu@gmail.com>
2023-12-04 22:53:06 +0100

    Isolated the SESSION parameters reset script in a file

---
+++ ajax_queries/reset_search_parameters.php
@@ -0,0 +1,9 @@
+<?php
+// Reset all the search parameters from animal_list.php stored in SESSION
+$_SESSION['sort'] = 'id_animal ASC';
+$_SESSION['name_filter'] = '';
+$_SESSION['breed_filter'] = '';
+$_SESSION['sex_filter'] = '';
+$_SESSION['current_page'] = 1;
+$_SESSION['rows_per_page'] = 10;
+?>
