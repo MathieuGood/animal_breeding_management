bd6a9a7328b58d7195433f2b3c34fcd24fc9f7a2
Mathieu <bon.mathieu@gmail.com>
2023-12-10 23:29:35 +0100

    Updated to getAllCompatiblePartners function to get the list of available partners

--- pages/breed_animals.php
+++ pages/breed_animals.php
@@ -5,35 +5,74 @@ if (isset($_SESSION['open']) && $_SESSION['open'] > 0) {
     $animal = new Animal();
     ?>
 
-    <select name='male_animal' value="---">
-        <option>Select male animal</option>
-        <?php
-        // Get the list of all male animal names
-        $males = $animal->getAllAliveAnimalsBySex('M');
+    <div class="row">
 
-        foreach ($males as $male) {
-            echo '<option ';
-            echo 'value="' . $male['id_animal'] . '">#' . $male['id_animal'] . ' ' . $male['animal_name'] . '</option>';
-        }
-        ?>
+        <div class="col-auto">
 
-    </select>
+            <select name='male_animal' id='select_male' value="---">
+                <option>Select male animal</option>
+                <?php
+                // Get the list of all male animal names
+                $males = $animal->getAllCompatiblePartners('M', $id = '');
 
-    <select name='female_animal' value="---">
-        <option value='0'>Select female animal</option>
-        <?php
-        // Get the list of all male animal names
-        $females = $animal->getAllAliveAnimalsAndParentsBySex('F');
+                foreach ($males as $male) {
+                    echo '<option value="' . $male['id_animal'] . '">
+                            #' . $male['id_animal'] . ' ' . $male['animal_name'] . ' > ' . $male['breed_name'] . 
+                        '</option>';
+                }
+                ?>
 
-        foreach ($females as $female) {
-            echo '<option ';
-            echo 'value="' . $female['id_animal'] . '">#' . $female['id_animal'] . ' ' . $female['animal_name'] . '</option>';
-        }
-        ?>
+            </select>
 
-    </select>
+        </div>
 
-    <input class="button" type="submit" name="start_mating" value="Animal mating">
+
+        <div class="col-auto">
+
+            <select name='female_animal' id='select_female' value="---">
+                <option value='0'>Select female animal</option>
+                <?php
+                // Get the list of all male animal names
+                $females = $animal->getAllCompatiblePartners('F', $id = '');
+
+                foreach ($females as $female) {
+                    echo '<option ';
+                    echo 'value="' . $female['id_animal'] . '">
+                     #' . $female['id_animal'] . ' ' . $female['animal_name'] . ' > ' . $female['breed_name'] . '</option>';
+                }
+                ?>
+            </select>
+
+            <div class="row">
+
+                <input class="button" type="submit" name="start_mating" value="Animal mating">
+
+            </div>
+
+        </div>
+
+    </div>
+    
+    <script>
+
+        let select_male = document.getElementById('select_male')
+        console.log(select_male)
+        let select_female = document.getElementById('select_female')
+        console.log(select_female)    
+        
+        select_male.addEventListener('change', () => {
+            // updateCompatiblePartners(select_male.value)
+            console.log(select_male.value)
+        })
+
+        select_female.addEventListener('change', () => {
+            // updateCompatiblePartners(select_male.value)
+            console.log(select_female.value)
+        })
+
+
+
+        </script>
 
     <?php
 
