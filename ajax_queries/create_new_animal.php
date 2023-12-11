0853b4811876d714001fe2e8b0d92cde8f68ff81
Mathieu <bon.mathieu@gmail.com>
2023-12-12 00:18:16 +0100

    Added link to reload page on button

--- ajax_queries/create_new_animal.php
+++ ajax_queries/create_new_animal.php
@@ -7,7 +7,8 @@ $breed = $_POST['id_breed'];
 $id_father = $_POST['id_father'];
 $id_mother = $_POST['id_mother'];
 
-var_dump($_POST);
+//For debugging
+// var_dump($_POST);
 
 
 
@@ -15,5 +16,22 @@ $animal = new Animal();
 
 $new_animal = $animal->createRandomAnimal(1, $breed, $id_father, $id_mother);
 
+$animal = new Animal($new_animal);
+
+$animal_name = $animal->getAnimalName();
 
 ?>
+
+<div class="card text-center mb-3" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Welcome to the world
+            <span id="snake_name">
+                <?php echo $animal_name ?>
+            </span>
+            !
+        </h5><br>
+        <img class="img-fluid" src="images/baby_snake.svg">
+        <p class="card-text"></p>
+        <a href="index.php?page=breed_animals" id="go_back" class="btn btn-primary">Back to breeding</a>
+    </div>
+</div>
