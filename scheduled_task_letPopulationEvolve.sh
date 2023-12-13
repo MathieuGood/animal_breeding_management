ded0523c52bed3df19262d7e27240130c5487a3f
Mathieu <bon.mathieu@gmail.com>
2023-12-13 14:46:20 +0100

    Created scheduled task launcher to run let_population_evolve.php.

---
+++ scheduled_task_letPopulationEvolve.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Starting population evolution for Breeding Manager"
+
+while true; do
+    echo 
+    current_time=$(date +%Y-%m-%d' '%H:%M:%S)
+    echo "Updated population on" $current_time 
+
+    sleep 2
+    
+    elapsed=$((current_time - start_time))
+
+done
