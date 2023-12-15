027b6e3a518d48c3f08667b2250c4f1c13aa37cb
Mathieu <bon.mathieu@gmail.com>
2023-12-15 10:37:48 +0100

    Added comments and cleaned file from unused code

--- classes/dbconnect.class.php
+++ classes/dbconnect.class.php
@@ -1,44 +1,48 @@
 <?php
 
+// dbConnect class to connect to the MariaDB database
+
 class dbConnect
 {
-
     private $host;
     private $dbname;
     private $user;
     private $passw;
     private $connect;
 
-    // Constructeur
+    // Connection parameters
     public function __construct($h = 'localhost', $db = 'breedingManager', $u = 'mariadb', $pw = 'mariadb*1')
     {
         $this->connect = new PDO("mysql:host=" . $h . ";dbname=" . $db, $u, $pw);
         $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    // Execution requête
+    // Method to check a query to avoid SQL injection and send it to the database
     public function sendQuery($query, $fetch_type = "both")
     {
         $startquery = explode(' ', trim($query));
+        // Only allow SELECT, INSERT, UPDATE, DELETE and CALL queries
         if ($startquery[0] == 'SELECT' || $startquery[0] == 'INSERT' || $startquery[0] == 'UPDATE' || $startquery[0] == 'DELETE' || $startquery[0] == 'CALL') {
-            // Exécution
+
             $result = $this->connect->query($query);
-            // Traitement du résultat
-            // Dans le cas d'un SELECT, on convertit le résulat de la queryuête en tableau PHP
+
+            // In case of a SELECT query, fetch the results
             if ($startquery[0] == 'SELECT' || str_contains($startquery[1], 'get')) {
-                
+
                 if ($fetch_type == "num") {
                     $result = $result->fetchAll(PDO::FETCH_ASSOC);
                 } else {
                     $result = $result->fetchAll();
                 }
             }
-            // Dans le cas d'un INSERT, on récupère l'id du nouvel élément créé dans la base
-            if ($startquery[0] == 'INSERT') {
 
+
+            if ($startquery[0] == 'INSERT') {
+                // In case of an INSERT query, return the last inserted ID
                 $result = $this->connect->lastInsertId();
 
             } else if (str_contains($startquery[1], 'create')) {
+                // In the particular calling createRandomAnimal() procedure, get the last inserted ID with a different query
 
                 $result = $this->connect->query("SELECT MAX(id_animal) AS last_insert FROM animal");
                 $result = $result->fetchAll(PDO::FETCH_ASSOC)[0]['last_insert'];
@@ -51,38 +55,27 @@ class dbConnect
     }
 
 
-    // Insert
-    // execInsert($table, ['col1', 'col2', 'col3,], ['value1', value2', 'value3'])
+    // Insert multiple values in a table in one query
+    // Method format : insertMultiple($table, ['col1', 'col2', 'col3,], ['value1', value2', 'value3'])
     public function insertMultiple($table, $cols, $values)
     {
-        // Boucle sur l'array $cols qui contient toutes les colonnes à traiter
-        // On transforme l'array en une string au bon format pour la requête SQL
-        // Format de sortie : `col1`, `col2`, `col3`
+
         $cols_list = '';
         foreach ($cols as $col) {
             $cols_list = $cols_list . "`" . $col . "`, ";
         }
         $cols_list = rtrim($cols_list, ", ");
-        // For debugging
-        // echo "Columns list : ".$cols_list."<br />";
 
-        // Même principe sur l'array $values
-        // Format de sortie : "value1", "value2", "value3"
         $values_list = '';
         foreach ($values as $value) {
             $values_list = $values_list . '"' . $value . '", ';
         }
         $values_list = rtrim($values_list, ", ");
-        // echo "Values list : ".$values_list."<br />";
 
         $query = "INSERT INTO `" . $table . "` (" . $cols_list . ") VALUES (" . $values_list . ")";
 
-        // For debugging
-        // echo "SQL Query : ".$query."<br />";
-
         $result = $this->sendQuery($query);
-        // echo "<br> Result  of insert multiple : ";
-        // var_dump($result);
+
         return $result;
     }
 
@@ -90,13 +83,10 @@ class dbConnect
     {
         $query = "UPDATE `" . $table . "` SET `" . $col . "` = '" . $value . "' WHERE `id_" . $table . "` = '" . $id . "'";
 
-        // For debugging
-        // echo $query."<br>";
-
         $this->sendQuery($query);
     }
 
-
+    // Get the column names of a table
     public function getColumnNames($table)
     {
         $query = "SELECT id_column_label, label
@@ -108,18 +98,6 @@ class dbConnect
                                  ON ids.COLUMN_NAME = column_label.id_column_label;";
         return $this->sendQuery($query);
     }
-
-    public function getColumnNamesAndInputType($table)
-    {
-        $query = "SELECT label, html_input_type
-                    FROM (SELECT DISTINCT COLUMN_NAME 
-                            FROM INFORMATION_SCHEMA.COLUMNS 
-                            WHERE TABLE_NAME = '" . $table . "')
-                            as ids
-                         INNER JOIN column_label
-                                 ON ids.COLUMN_NAME = column_label.id_column_label;";
-        return $this->sendQuery($query);
-    }
-
 }
+
 ?>
